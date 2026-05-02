9845c99a0dc71369dd38f8c9fbfa2391180d5a73
Denys Vitali <denys@denv.it>
2026-05-02 19:53:10 +0000

    feat(observability): connect glitchtip

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:trip_viewer/pages/trip_list.dart';
 import 'package:trip_viewer/pages/settings.dart';
 import 'package:trip_viewer/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  await SentryFlutter.init(
+    (options) => options
+      ..dsn = 'https://0c4c5560da4841dcb0271da750b5dfea@glitchtip.k2.k8s.best/6'
+      ..tracesSampleRate = 0.01
+      ..enableAutoSessionTracking = false,
+    appRunner: () => runApp(const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,6 +26,9 @@ class MyApp extends StatelessWidget {
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
       home: const TripListPage(),
+      navigatorObservers: [
+        SentryNavigatorObserver(),
+      ],
       routes: {
         '/settings': (context) => const SettingsPage(),
       },
